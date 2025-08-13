d2d42d95929cd35bf662c8f4e354d8681d818bac
Serhii Chumak <s4umak@gmail.com>
2025-08-13 12:40:13 +0300

    Solution 2 py herbivores and carnivores

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class AliveList(list):
-    def __repr__(self):
+    def __repr__(self) -> None:
         formatted_animals = [
             {
                 "Name": animal.name,
@@ -12,48 +12,54 @@ class AliveList(list):
 
 
 class Animal:
-    alive = []
+    alive = AliveList()
 
-    def __init__(self, name, health = 100, hidden = False):
-
+    def __init__(self,
+                 name: str,
+                 health: int = 100,
+                 hidden: bool = False
+                 ) -> None:
         self.name = name
         self._health = health
         self.hidden = hidden
         Animal.alive.append(self)
 
     @property
-    def health(self):
+    def health(self) -> int:
         return self._health
 
     @health.setter
-    def health(self, value):
+    def health(self, value: int) -> None:
         self._health = max(0, value)
         if self._health == 0:
             self._die()
 
-    def _die(self):
+    def _die(self) -> None:
         Animal.alive.remove(self)
 
-    def __repr__(self):
-        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
-
+    def __repr__(self) -> str:
+        return (f"{{Name: " 
+                f"{self.name}, "
+                f"Health: {self.health}, "
+                f"Hidden: {self.hidden}}}"
+                )
 
 
 class Herbivore(Animal):
 
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
-        print(f"{self.name} now is {'hidden' if self.hidden else 'no hidden'}.")
+        print(f"{self.name} now is"
+              f"{"hidden" if self.hidden else "no hidden"}.")
 
 
 class Carnivore(Animal):
-
-    def bite(self, target):
-
+    def bite(self, target: str) -> None:
         if isinstance(target, Herbivore) and not target.hidden:
             damage = 50
             target.health -= damage
-            print(f"{self.name} bite {target.name}! Health {target.name}: {target.health}")
+            print(f"{self.name} bite {target.name}!"
+                  f"Health {target.name}: {target.health}")
         elif isinstance(target, Carnivore):
             print(f"{self.name} can't bite other carnivore.")
         elif target.hidden:
